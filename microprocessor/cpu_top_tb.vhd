889934bd073c65843967943113733ae3190d5f40
Cristian Salitre <126845001+Salitre97@users.noreply.github.com>
2024-04-21 16:03:29 -0400

    Update cpu_top_tb.vhd

--- microprocessor/cpu_top_tb.vhd
+++ microprocessor/cpu_top_tb.vhd
@@ -1,84 +1,72 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/19/2024 11:48:12 AM
--- Design Name: 
--- Module Name: cpu_top_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity cpu_top_tb is
---  Port ( );
+-- The testbench entity does not have ports.
 end cpu_top_tb;
 
-architecture Behavioral of cpu_top_tb is
+architecture testbench of cpu_top_tb is
+ 
+  signal clk      : std_logic := '0';
+  signal clk50    : std_logic := '0';
+  signal reset    : std_logic := '0';
+  signal an       : std_logic_vector(3 downto 0);
+  signal ca, cb, cc, cd, ce, cf, cg : std_logic;
 
-component cpu_top is
+  -- Clock period constants
+  constant clk_period : time := 10 ns;
+  constant clk50_period : time := 20 ns;
+
+  -- Component declaration for cpu_top (if you have a separate file)
+  component cpu_top
     port(
-        clk : in std_logic;
-        reset : in std_logic;
-        wr_en : out std_logic;
-        dr : in std_logic_vector(7 downto 0); -- Data from the memory
-        dw : out std_logic_vector(7 downto 0); -- Data to the memory
-        addr : out std_logic_vector(7 downto 0); -- Memory address
-        pc_out : out std_logic_vector(7 downto 0); -- Program counter value
-        accu_out : out std_logic_vector(7 downto 0) -- Accumulator value
+      clk      : in std_logic;
+      clk50    : in std_logic;
+      reset    : in std_logic;
+      an       : out std_logic_vector(3 downto 0);
+      ca       : out std_logic;
+      cb       : out std_logic;
+      cc       : out std_logic;
+      cd       : out std_logic;
+      ce       : out std_logic;
+      cf       : out std_logic;
+      cg       : out std_logic
     );
-end component;
-
-signal clk : std_logic := '0';
-signal reset : std_logic;
-signal  wr_en : std_logic;
-signal  dr : std_logic_vector(7 downto 0); -- Data from the memory
-signal  dw : std_logic_vector(7 downto 0); -- Data to the memory
-signal  addr : std_logic_vector(7 downto 0); -- Memory address
-signal pc_out : std_logic_vector(7 downto 0); -- Program counter value
-signal accu_out : std_logic_vector(7 downto 0);
+  end component;
 
 begin
+  -- Instantiate the cpu_top component
+  uut: cpu_top
+    port map (
+      clk      => clk,
+      clk50    => clk50,
+      reset    => reset,
+      an       => an,
+      ca       => ca,
+      cb       => cb,
+      cc       => cc,
+      cd       => cd,
+      ce       => ce,
+      cf       => cf,
+      cg       => cg
+    );
 
-    uut: cpu_top port map (
-        clk => clk,
-        reset => reset,
-        wr_en => wr_en,
-        dr => dr,
-        dw => dw,
-        addr => addr,
-        pc_out => pc_out,
-        accu_out => accu_out);
-    
-    clk_process: process
-    begin 
-        clk <= '0';
-        wait for 50 ns;
-        clk <= '1';
-        wait for 50ns;
-    end process;  
-   
+  -- Clock generation process for system clock
+  clk_process: process
+  begin
+      clk <= '1';
+      wait for 50ps;
+      clk <= '0';
+      wait for 50ps;
+  end process;
 
-
-end Behavioral;
+  -- Clock generation process for display clock
+  clk50_process: process
+  begin
+      clk50 <= '1';
+      wait for 100ps;
+      clk50 <= '0';
+      wait for 100ps;
+  end process;
+end testbench;
